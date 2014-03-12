a35eea2791bbb550d744a517b2b0056accb08e71
Kathy <kathyw268@gmail.com>
2014-03-12 21:04:38 +0000

    ?

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Hoardr::Application.routes.draw do
   resources :items
 
   root 'items#index'
+  get     "users"       => "users#show",        as: "users"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
